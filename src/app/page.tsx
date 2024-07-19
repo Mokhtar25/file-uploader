0d96724dcb762c0e5941862da9ae940453fb558e
mohtar25 <mokhtar2000naj@gmail.com>
2024-07-19 19:01:15 +0200

    sign in protection to see files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Link from "next/link";
-import Image from "next/image";
 import { db } from "~/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
@@ -8,14 +7,17 @@ export default async function HomePage() {
   const data = await db.query.files.findMany();
   return (
     <main className="">
-      <div className="flex flex-wrap">
-        {data.map((e) => (
-          <div className="flex flex-col" key={e.id}>
-            <img src={e.url} alt="" />
-            <span>{e.name}</span>
-          </div>
-        ))}
-      </div>
+      <SignedOut>please sign in above</SignedOut>
+      <SignedIn>
+        <div className="flex flex-wrap">
+          {data.map((e) => (
+            <div className="flex flex-col" key={e.id}>
+              <img src={e.url} alt="" />
+              <span>{e.name}</span>
+            </div>
+          ))}
+        </div>
+      </SignedIn>
     </main>
   );
 }
