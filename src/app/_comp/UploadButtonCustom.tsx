6cf3e97d508e3a94ecb00fd6d7f46d4388169b4d
mohtar25 <mokhtar2000naj@gmail.com>
2024-07-22 12:54:21 +0200

    styling sonner and adding spinner for uploading

--- src/app/_comp/UploadButtonCustom.tsx
+++ src/app/_comp/UploadButtonCustom.tsx
@@ -26,7 +26,7 @@ export default function MultiUploader() {
       alert("error occurred while uploading");
     },
     onUploadBegin: () => {
-      toast("Uploading", {
+      toast(<FileUp />, {
         duration: 90000,
         id: "upload-start",
       });
@@ -57,3 +57,28 @@ export default function MultiUploader() {
     </div>
   );
 }
+
+const FileUp = () => {
+  return (
+    <div className="flex items-center gap-2 text-base">
+      <LoadingSpinner />
+      <span>Uploading...</span>
+    </div>
+  );
+};
+
+const LoadingSpinner = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      stroke="#000"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g className="spinner_V8m1">
+        <circle cx="12" cy="12" r="9.5" fill="none" stroke-width="3"></circle>
+      </g>
+    </svg>
+  );
+};
