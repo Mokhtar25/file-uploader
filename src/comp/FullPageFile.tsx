b76a1e8e02f69433a4589055321c67e8e417ea9e
mohtar25 <mokhtar2000naj@gmail.com>
2024-07-23 13:56:34 +0200

    edting the fullpage

--- src/comp/FullPageFile.tsx
+++ src/comp/FullPageFile.tsx
@@ -6,7 +6,7 @@ export default async function FullPageFile({ id }: { id: number }) {
   return (
     <div
       style={{ backgroundImage: `url("${image.url}")` }}
-      className="absolute inset-0 left-0 right-0 top-20 mx-auto flex h-3/4 w-3/4 min-w-0 flex-shrink items-center justify-center rounded bg-red-500 bg-cover bg-center"
+      className="absolute inset-0 left-0 right-0 top-20 mx-auto flex h-3/4 w-3/4 min-w-0 flex-shrink items-center justify-center rounded bg-slate-300 bg-cover bg-center shadow-lg shadow-slate-700"
     ></div>
   );
 }
