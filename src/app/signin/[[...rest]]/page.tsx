2751b7ef758625b1afd4b7dade26f77cddb7e12a
mohtar25 <mokhtar2000naj@gmail.com>
2024-07-24 14:10:24 +0200

    sign up page instead of signin ;

--- src/app/signin/[[...rest]]/page.tsx
+++ src/app/signin/[[...rest]]/page.tsx
@@ -1,4 +1,4 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, SignUp } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -9,11 +9,13 @@ export default function page() {
   return (
     <div className="absolute inset-0 flex h-screen w-screen items-center justify-center gap-16 bg-slate-100">
       <div className="h-[460px] w-[400px]">
-        <SignIn />
+        <SignUp />
       </div>
-      <div className="flex size-96 flex-col gap-12 rounded text-5xl font-bold text-slate-800 shadow-black">
+      <div className="mt-20 flex size-96 flex-col gap-12 rounded text-5xl font-bold text-slate-800 shadow-black">
         <span>Easy and secure access to your content</span>
-        <span className="text-3xl text-slate-500">Try it Today for free</span>
+        <span className="text-3xl font-medium text-slate-500">
+          Try it Today for free
+        </span>
       </div>
     </div>
   );
